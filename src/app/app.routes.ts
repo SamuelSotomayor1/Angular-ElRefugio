0d2f625c752018edfba204d095ecbc5e1ebee59d
SamuelSotomayor1 <andressotomayor477@gmail.com>
2025-09-02 03:55:45 -0400

    feat: new components are created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Menu } from './pages/menu/menu';
+import { Reservas } from './pages/reservas/reservas';
+import { Nosotros } from './pages/nosotros/nosotros';
 
 export const routes: Routes = [
     {path: '', component: Home},
     {path: 'menu', component: Menu},
+    {path: 'reservas', component: Reservas},
+    {path: 'nosotros', component: Nosotros},
 ];
