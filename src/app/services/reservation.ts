f638aa54f1bd4c3fad02aaa18c275018876a575c
SamuelSotomayor1 <andressotomayor477@gmail.com>
2025-09-03 02:10:51 -0400

    feat: the reservation service are created

---
+++ src/app/services/reservation.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ReservationService {
+  private apiUrl = 'http://localhost:3000/api/reservation'; 
+
+  constructor(private http: HttpClient) {}
+
+  createReservation(data: any): Observable<any> {
+    return this.http.post(this.apiUrl, data);
+  }
+
+  getReservations(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+}
